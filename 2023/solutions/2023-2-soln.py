21b064a26065f3dee6a9b4aacd687006617749c5
Taylor Gregory <35243041+taylorgregory@users.noreply.github.com>
2023-12-03 21:12:56 +1000

    improved day 2 part a

--- 2023/solutions/2023-2-soln.py
+++ 2023/solutions/2023-2-soln.py
@@ -1,26 +1,25 @@
 with open('../inputs/2023-2-input.txt') as file:
     input_arr = file.read().splitlines()
 
+colours_in_bag =  {
+    "red": 12,
+    "green": 13,
+    "blue": 14
+}
+
 id_total = 0
 
 for i in range(len(input_arr)):
-    valid_draws = 0
-    # Assumes that a single draw doesn't have two records for a single colour
     game_data = input_arr[i].split(': ')[1].replace(';', ',').split(', ')
+    valid_draw = True
 
     for j in range(len(game_data)):
         current_draw = game_data[j].split(' ')
-        # Should use mapping here
-        # Should also break if invalid draw exists
-        if (current_draw[1] == 'red' and int(current_draw[0]) <= 12) or (current_draw[1] == 'green' and int(current_draw[0]) <= 13) or (current_draw[1] == 'blue' and int(current_draw[0]) <= 14):
-            valid_draws += 1
-    
-    if valid_draws == len(game_data):
+        if int(current_draw[0]) > colours_in_bag[current_draw[1]]:
+            valid_draw = False
+            break
+        
+    if valid_draw == True:
         id_total += i + 1
 
 print(id_total)
-    
-
-
-
-
