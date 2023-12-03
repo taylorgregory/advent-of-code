e663c57a03483925ca181180cdcd6f5eca6fbb0b
taylorgregory <taylorgrgry@gmail.com>
2023-12-03 15:05:17 +1000

    added very temporary day 1 part b

--- 2023/solutions/2023-1-soln.py
+++ 2023/solutions/2023-1-soln.py
@@ -1,4 +1,5 @@
 import re
+import num2words
 
 with open('../inputs/2023-1-input.txt') as file:
     input_arr = file.read().splitlines()
@@ -6,8 +7,23 @@ with open('../inputs/2023-1-input.txt') as file:
 rolling_total = 0
 
 for i in range(len(input_arr)):
-    filtered_row = re.sub('\D', '', input_arr[i])
-    rolling_total += int(filtered_row[0] + filtered_row[len(filtered_row) - 1])
+
+    # TOGGLE COMMENT FOR PART A
+    #filtered_row = re.sub('\D', '', input_arr[i])
+    # END PART A
+
+    # TOGGLE COMMENT FOR PART B
+    this_row = input_arr[i]
+    # HATE this (but works for now).......... TBC:
+    words_to_nums = {'one': 'one1one', 'two': 'two2two', 'three': 'three3three', 'four': 'four4four', 'five': 'five5five', 'six': 'six6six', 'seven': 'seven7seven', 'eight': 'eight8eight', 'nine': 'nine9nine'}
+    for key, value in words_to_nums.items():
+        this_row = this_row.replace(key, value)
+    filtered_row = re.sub('\D', '', this_row)
+    # END PART B
+
+    first_num = filtered_row[0]
+    last_num = filtered_row[len(filtered_row) - 1]
+    rolling_total += int(first_num + last_num)
 
 print(rolling_total)
 
