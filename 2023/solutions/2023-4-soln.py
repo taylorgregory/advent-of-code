a9ea360a840421177b707e0887d2b90f5cc31a41
taylorgregory <taylorgrgry@gmail.com>
2024-02-15 20:30:09 +1000

    cleaned day 4 part a

--- 2023/solutions/2023-4-soln.py
+++ 2023/solutions/2023-4-soln.py
@@ -4,18 +4,16 @@ with open('../inputs/2023-4-input.txt') as file:
 total_counter = 0
 
 for i in range(len(input_arr)):
-    winning_numbers = input_arr[i].split(": ")[1].split(" | ")[0].replace("  ", " ").strip()
-    your_numbers = input_arr[i].split(": ")[1].split(" | ")[1].replace("  ", " ").strip()
-    winning_numbers_arr = winning_numbers.split(" ")
-    your_numbers_arr = your_numbers.split(" ")
+    winning_numbers = input_arr[i].split(": ")[1].split(" | ")[0].replace("  ", " ").strip().split(" ")
+    your_numbers = input_arr[i].split(": ")[1].split(" | ")[1].replace("  ", " ").strip().split(" ")
 
     counter = 0
-    for your_number in your_numbers_arr:
-        if your_number in winning_numbers_arr:
+    for your_number in your_numbers:
+        if your_number in winning_numbers:
             if counter == 0:
                 counter = 1
             else:
-                counter = counter * 2
+                counter *= 2
 
     total_counter += counter
 
