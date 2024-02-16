6b45af5f632380d7d47970099a229f4b33eff276
taylorgregory <taylorgrgry@gmail.com>
2024-02-17 08:45:30 +1000

    Removed duplicate test data

--- aoc2023/2023-2-soln.py
+++ aoc2023/2023-2-soln.py
@@ -31,7 +31,7 @@ def part_b(input):
             "green": 0,
             "blue": 0
         }
-        
+
         game_data = row.split(': ')[1].replace(';', ',').split(', ')
         
         for data in game_data:
@@ -44,13 +44,7 @@ def part_b(input):
     return power_total
 
 if __name__ == "__main__":
-    test_data_a = '''Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
-Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
-Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
-Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
-Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green'''.splitlines()
-    
-    test_data_b = '''Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+    test_data = '''Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
 Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
 Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
 Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
@@ -59,8 +53,8 @@ Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green'''.splitlines()
     data = get_data(day=2, year=2023).splitlines()
 
     # Run tests for Part A and Part B
-    assert part_a(test_data_a) == 8
-    assert part_b(test_data_b) == 2286
+    assert part_a(test_data) == 8
+    assert part_b(test_data) == 2286
 
     # Submit answers for Part A and Part B
     submit(part_a(data), part="a", day=2, year=2023)    
