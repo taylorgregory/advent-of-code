6ca6df9881511b395ede2c8c11eab16eb92048dd
taylorgregory <taylorgrgry@gmail.com>
2024-02-16 16:54:01 +1000

    Refactored 2023 day 1

--- aoc2023/2023-1-soln.py
+++ aoc2023/2023-1-soln.py
@@ -1,28 +1,41 @@
 import re
-from aocd import get_data
+from aocd import get_data, submit
 
-input_arr = get_data(day=1, year=2023).splitlines()
+def part_a(input):
+    rolling_total = 0
+    for row in input:
+        filtered_row = re.sub('\D', '', row)
+        first_num = filtered_row[0]
+        last_num = filtered_row[len(filtered_row) - 1]
+        rolling_total += int(first_num + last_num)
 
-rolling_total = 0
+    return rolling_total
 
-for i in range(len(input_arr)):
+def part_b(input):
+    rolling_total = 0
+    for i in range(len(input)):
+        this_row = input[i]
+        # HATE this (but works for now)..TBC:
+        words_to_nums = {'one': 'one1one', 'two': 'two2two', 'three': 'three3three', 'four': 'four4four', 'five': 'five5five', 'six': 'six6six', 'seven': 'seven7seven', 'eight': 'eight8eight', 'nine': 'nine9nine'}
+        for key, value in words_to_nums.items():
+            this_row = this_row.replace(key, value)
 
-    # TOGGLE COMMENT FOR PART A
-    #filtered_row = re.sub('\D', '', input_arr[i])
-    # END PART A
+        filtered_row = re.sub('\D', '', this_row)
+        first_num = filtered_row[0]
+        last_num = filtered_row[len(filtered_row) - 1]
+        rolling_total += int(first_num + last_num)
 
-    # TOGGLE COMMENT FOR PART B
-    this_row = input_arr[i]
-    # HATE this (but works for now).......... TBC:
-    words_to_nums = {'one': 'one1one', 'two': 'two2two', 'three': 'three3three', 'four': 'four4four', 'five': 'five5five', 'six': 'six6six', 'seven': 'seven7seven', 'eight': 'eight8eight', 'nine': 'nine9nine'}
-    for key, value in words_to_nums.items():
-        this_row = this_row.replace(key, value)
-    filtered_row = re.sub('\D', '', this_row)
-    # END PART B
+    return rolling_total
 
-    first_num = filtered_row[0]
-    last_num = filtered_row[len(filtered_row) - 1]
-    rolling_total += int(first_num + last_num)
+if __name__ == "__main__":
+    input_arr = get_data(day=1, year=2023).splitlines()
 
-print(rolling_total)
-
+    # Part A
+    part_a_answer = part_a(input_arr)
+    print("Submitting " + str(part_a_answer) + " for Part A...")
+    submit(part_a_answer, part="a", day=1, year=2023)
+    
+    # Part B
+    part_b_answer = part_b(input_arr)
+    print("Submitting " + str(part_b_answer) + " for Part B...")
+    submit(part_b_answer, part="b", day=1, year=2023)
