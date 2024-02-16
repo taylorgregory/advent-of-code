684652f68110ab471e30f8726730508ba9b9e384
taylorgregory <taylorgrgry@gmail.com>
2024-02-16 23:17:32 +1000

    Refactored Day 2 2023

--- aoc2023/2023-2-soln.py
+++ aoc2023/2023-2-soln.py
@@ -1,48 +1,57 @@
-from aocd import get_data
+from aocd import get_data, submit
 
-input_arr = get_data(day=2, year=2023).splitlines()
-
-# PART A
-colours_in_bag =  {
-    "red": 12,
-    "green": 13,
-    "blue": 14
-}
-
-id_total = 0
-
-for i in range(len(input_arr)):
-    game_data = input_arr[i].split(': ')[1].replace(';', ',').split(', ')
-    valid_draw = True
-
-    for j in range(len(game_data)):
-        current_draw = game_data[j].split(' ')
-        if int(current_draw[0]) > colours_in_bag[current_draw[1]]:
-            valid_draw = False
-            break
-        
-    if valid_draw:
-        id_total += i + 1
-
-print(id_total)
-
-# PART B
-power_total = 0
-
-for i in range(len(input_arr)):
-    game_data = input_arr[i].split(': ')[1].replace(';', ',').split(', ')
-    
-    max_cols =  {
-        "red": 0,
-        "green": 0,
-        "blue": 0
+def part_a(input):
+    colours_in_bag =  {
+        "red": 12,
+        "green": 13,
+        "blue": 14
     }
-    
-    for j in range(len(game_data)):
-        current_draw = game_data[j].split(' ')   
-        if int(current_draw[0]) > max_cols[current_draw[1]]:
-            max_cols[current_draw[1]] = int(current_draw[0])
+
+    id_total = 0
+    for i, row in enumerate(input):
+        game_data = row.split(': ')[1].replace(';', ',').split(', ')
+        valid_draw = True
+
+        for data in game_data:
+            current_draw = data.split(' ')
+            if int(current_draw[0]) > colours_in_bag[current_draw[1]]:
+                valid_draw = False
+                break
             
-    power_total += max_cols['red'] * max_cols['green'] * max_cols['blue']
+        if valid_draw:
+            id_total += i + 1
+
+    return id_total
+
+def part_b(input):
+    power_total = 0
+    for row in input:
+        game_data = row.split(': ')[1].replace(';', ',').split(', ')
+        
+        max_cols =  {
+            "red": 0,
+            "green": 0,
+            "blue": 0
+        }
+        
+        for data in game_data:
+            current_draw = data.split(' ')   
+            if int(current_draw[0]) > max_cols[current_draw[1]]:
+                max_cols[current_draw[1]] = int(current_draw[0])
+                
+        power_total += max_cols['red'] * max_cols['green'] * max_cols['blue']
+        
+    return power_total
+
+if __name__ == "__main__":
+    input_arr = get_data(day=2, year=2023).splitlines()
+
+    # Part A
+    part_a_answer = part_a(input_arr)
+    print("Submitting " + str(part_a_answer) + " for Part A...")
+    submit(part_a_answer, part="a", day=2, year=2023)
     
-print(power_total)
+    # Part B
+    part_b_answer = part_b(input_arr)
+    print("Submitting " + str(part_b_answer) + " for Part B...")
+    submit(part_b_answer, part="b", day=2, year=2023)
