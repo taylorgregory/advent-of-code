83479bed7885ab6dd8ea4d89240d8c8f0897a938
taylorgregory <taylorgrgry@gmail.com>
2024-02-18 16:48:52 +1000

    Refactored Day 2 2023

--- 2023/2023-2-soln.py
+++ 2023/2023-2-soln.py
@@ -1,5 +1,12 @@
 from aocd import get_data, submit
 
+def format_data(input_str):
+    data = []
+    for row in input_str.splitlines():
+        data.append(row.split(': ')[1].replace(';', ',').split(', '))
+    return data
+
+
 def part_a(input):
     colours_in_bag =  {
         "red": 12,
@@ -7,21 +14,19 @@ def part_a(input):
         "blue": 14
     }
 
-    id_total = 0
+    total = 0
     for i, row in enumerate(input):
-        game_data = row.split(': ')[1].replace(';', ',').split(', ')
         valid_draw = True
-
-        for data in game_data:
-            current_draw = data.split(' ')
-            if int(current_draw[0]) > colours_in_bag[current_draw[1]]:
+        for draw in row:
+            number, colour = draw.split(' ')
+            if int(number) > colours_in_bag[colour]:
                 valid_draw = False
                 break
             
         if valid_draw:
-            id_total += i + 1
+            total += i + 1
 
-    return id_total
+    return total
 
 def part_b(input):
     power_total = 0
@@ -32,10 +37,8 @@ def part_b(input):
             "blue": 0
         }
 
-        game_data = row.split(': ')[1].replace(';', ',').split(', ')
-        
-        for data in game_data:
-            this_number, this_colour = data.split(' ')   
+        for draw in row:
+            this_number, this_colour = draw.split(' ')   
             if int(this_number) > max_cols[this_colour]:
                 max_cols[this_colour] = int(this_number)
                 
@@ -44,19 +47,18 @@ def part_b(input):
     return power_total
 
 if __name__ == "__main__":
-    # Get all data
-    test_data = '''Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+    # Testing
+    test_string = '''Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
 Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
 Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
 Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
-Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green'''.splitlines()
-
-    data = get_data(day=2, year=2023).splitlines()
-
-    # Run tests for Part A and Part B
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green'''
+    
+    test_data = format_data(test_string)
     assert part_a(test_data) == 8
     assert part_b(test_data) == 2286
 
-    # Submit answers for Part A and Part B
+    # Solve
+    data = format_data(get_data(day=2, year=2023))
     submit(part_a(data), part="a", day=2, year=2023)    
     submit(part_b(data), part="b", day=2, year=2023)
