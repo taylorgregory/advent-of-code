4a54648120022b2cecfb6e630e0fad3ebbeed236
taylorgregory <taylorgrgry@gmail.com>
2024-02-25 13:55:04 +1000

    Cleaner logic for Day 1 2023

--- 2023/2023-1-soln.py
+++ 2023/2023-1-soln.py
@@ -1,37 +1,29 @@
 import re
 from aocd import get_data, submit
+from words2num import w2n
 
 def part_a(input):
-    rolling_total = 0
-    for row in input:
-        filtered_row = re.sub('\D', '', row)
-        first_num = filtered_row[0]
-        last_num = filtered_row[len(filtered_row) - 1]
-        rolling_total += int(first_num + last_num)
-
-    return rolling_total
+    filtered_rows = [re.sub('\D', '', x) for x in input.splitlines()]
+    return sum([int(x[0] + x[len(x)-1]) for x in filtered_rows])
 
 def part_b(input):
-    rolling_total = 0
-    for row in input:
-        # HATE this (but works for now)..TBC:
-        words_to_nums = {'one': 'one1one', 'two': 'two2two', 'three': 'three3three', 'four': 'four4four', 'five': 'five5five', 'six': 'six6six', 'seven': 'seven7seven', 'eight': 'eight8eight', 'nine': 'nine9nine'}
-        for key, value in words_to_nums.items():
-            row = row.replace(key, value)
+    updated_rows = []
+    for row in input.splitlines():
+        row_arr = [*row]
+        matches = re.finditer(f'(?=({"one|two|three|four|five|six|seven|eight|nine"}))', row)
+        for i, match in enumerate(matches):
+            row_arr.insert(match.start()+i, str(w2n(match.group(1))))
+        updated_rows.append("".join(row_arr))
 
-        filtered_row = re.sub('\D', '', row)
-        first_num = filtered_row[0]
-        last_num = filtered_row[len(filtered_row) - 1]
-        rolling_total += int(first_num + last_num)
-
-    return rolling_total
+    filtered_rows = [re.sub('\D', '', row) for row in updated_rows]
+    return sum([int(row[0] + row[len(row)-1]) for row in filtered_rows])
 
 if __name__ == "__main__":
     # Get all data
     test_data_a = '''1abc2
 pqr3stu8vwx
 a1b2c3d4e5f
-treb7uchet'''.splitlines()
+treb7uchet'''
     
     test_data_b = '''two1nine
 eightwothree
@@ -39,9 +31,9 @@ abcone2threexyz
 xtwone3four
 4nineeightseven2
 zoneight234
-7pqrstsixteen'''.splitlines()
+7pqrstsixteen'''
     
-    data = get_data(day=1, year=2023).splitlines()
+    data = get_data(day=1, year=2023)
     
     # Run tests for Part A and Part B
     assert part_a(test_data_a) == 142
