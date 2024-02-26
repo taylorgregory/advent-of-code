65a6c704d886d188f54ae0ef4384c2326e2f7ac9
taylorgregory <taylorgrgry@gmail.com>
2024-02-26 22:25:16 +1000

    Different approach for Day 8 2023

--- 2023/2023-8-soln.py
+++ 2023/2023-8-soln.py
@@ -1,5 +1,6 @@
 from aocd import get_data, submit
 import re
+import numpy as np
 
 def format_data(input_str):
     input_arr = input_str.splitlines()
@@ -13,47 +14,26 @@ def format_data(input_str):
     return instructions, dict
 
 def part_a(instructions, dict):
-    lr_dict = {
-        "L": 0,
-        "R": 1
-    }
-
-    location = "AAA"
-    mod_counter = 0
-    num_moves = 0
-    while location != "ZZZ":
-        location = dict[location][lr_dict[instructions[mod_counter % len(instructions)]]]
-        mod_counter += 1
-        num_moves += 1
-
-    return num_moves
+    moves = ["AAA"]
+    while moves[len(moves)-1] != "ZZZ":
+        left_right = 0 if instructions[(len(moves)-1) % len(instructions)] == "L" else 1
+        moves.append(dict[moves[len(moves)-1]][left_right])  
+    return len(moves)-1
 
 def part_b(instructions, dict):
-
-    lr_dict = {
-        "L": 0,
-        "R": 1
-    }
-    # Get all instructions that end in A
+    # Obtaining a list of all the A starting points
     a_ending = {key: val for key, val in dict.items() if re.search(f"A$", key)}
-
-    # Make result list that holds the locations for each 
     locations = list(a_ending.keys())
-    mod_counter = 0
-    num_moves = 0
-    num_z = 0
-    while num_z < len(locations):
-        num_z = 0
-        for i, location in enumerate(locations):
-            locations[i] = dict[location][lr_dict[instructions[mod_counter % len(instructions)]]]
-            if re.search(f"Z$", locations[i]):
-                num_z += 1
-        mod_counter += 1
-        num_moves += 1
-        print(num_moves)
-        print(num_z)
+    all_moves = []
 
-    return num_moves
+    for location in locations:
+        moves = [location]
+        while not re.search(f"Z$", moves[len(moves)-1]):
+            left_right = 0 if instructions[(len(moves)-1) % len(instructions)] == "L" else 1
+            moves.append(dict[moves[len(moves)-1]][left_right])  
+        all_moves.append(moves)
+
+    return np.lcm.reduce([len(x)-1 for x in all_moves])
 
 if __name__ == "__main__":
     # Testing
@@ -92,8 +72,8 @@ XXX = (XXX, XXX)'''
     assert part_a(test_moves_2, test_dict_2) == 6
 
     test_moves_3, test_dict_3 = format_data(test_string_3)
-    print(part_b(test_moves_3, test_dict_3))
+    assert part_b(test_moves_3, test_dict_3) == 6
 
     moves, dict = format_data(get_data(day=8, year=2023))
-    #submit(part_a(moves, dict), part="a", day=8, year=2023)  
-    print(part_b(moves, dict))
+    submit(part_a(moves, dict), part="a", day=8, year=2023)  
+    submit(part_b(moves, dict), part="b", day=8, year=2023) 
