397ff57f45c282a65f8b93f7d4594f8d962517d7
taylorgregory <taylorgrgry@gmail.com>
2024-04-27 13:16:46 +1000

    Used dedent for test data to improve Python formatting

--- 2023/2023-1-soln.py
+++ 2023/2023-1-soln.py
@@ -1,3 +1,4 @@
+from textwrap import dedent
 import re
 from aocd import get_data, submit
 from words2num import w2n
@@ -66,26 +67,30 @@ def part_b(input):
     return sum([int(row[0] + row[len(row)-1]) for row in filtered_rows])
 
 if __name__ == "__main__":
-    # Get all data
-    test_data_a = '''1abc2
-pqr3stu8vwx
-a1b2c3d4e5f
-treb7uchet'''
-    
-    test_data_b = '''two1nine
-eightwothree
-abcone2threexyz
-xtwone3four
-4nineeightseven2
-zoneight234
-7pqrstsixteen'''
-    
-    data = get_data(day=1, year=2023)
-    
-    # Run tests for Part A and Part B
-    assert part_a(test_data_a) == 142
-    assert part_b(test_data_b) == 281
 
-    # Submit answers for Part A and Part B
+    # Testing
+    test_data_a = dedent("""
+        1abc2
+        pqr3stu8vwx
+        a1b2c3d4e5f
+        treb7uchet
+    """).strip("\n")
+
+    assert part_a(test_data_a) == 142
+    
+    test_data_b = dedent("""
+        two1nine
+        eightwothree
+        abcone2threexyz
+        xtwone3four
+        4nineeightseven2
+        zoneight234
+        7pqrstsixteen
+    """).strip("\n")
+
+    assert part_b(test_data_b) == 281
+    
+    # Submission
+    data = get_data(day=1, year=2023)
     submit(part_a(data), part="a", day=1, year=2023)    
     submit(part_b(data), part="b", day=1, year=2023)
